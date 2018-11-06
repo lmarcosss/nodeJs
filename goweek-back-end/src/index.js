a30eaef8c2c6d6f6b849a0d9320936dac584bbb8
Leonardo Marcos <marcosleonardosss@gmail.com>
2018-11-06 11:24:43 -0200

    feat: new project nodeJs

---
+++ goweek-back-end/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb://goweek:goweek123@ds051378.mlab.com:51378/goweek-back-end",
+  { useNewUrlParser: true }
+);
+
+app.use(require("./routes"));
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
